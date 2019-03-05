6a2d7938edc291fff808a24a6e56673bf5e8a302
Raissa Amini <raissaamini@gmail.com>
2019-03-05 09:58:15 +0700

    strukdat-02

---
+++ exercise-01.cpp
@@ -0,0 +1,33 @@
+/*
+Kelas   :A
+Nama    :Raissa Amini
+NPM     :140810180073
+Tanggal :5 Maret 2019
+*/
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct Orang{
+    int umur;
+    char nama[30];
+    char jk[30];
+    char goldar[30];
+};
+
+int main()
+{
+    Orang orang;
+    orang.umur=10;
+    strcpy(orang.nama,"Fahmi");
+    strcpy(orang.jk,"L");
+    strcpy(orang.goldar,"O");
+
+    cout<< orang.umur<< endl;
+    cout<<orang.nama<< endl;
+    cout<<orang.goldar<< endl;
+    cout<<orang.jk<< endl;
+
+    return 0;
+}
---
+++ exercise-02.cpp
@@ -0,0 +1,29 @@
+/*
+Kelas   :A
+Nama    :Raissa Amini
+NPM     :140810180073
+Tanggal :5 Maret 2019
+*/
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+struct Theater{
+    int room;
+    char seat[3];
+    char movieTitle[30];
+};
+
+int main()
+{
+    Theater theater;
+    thater.room=7;
+    strcpy(theater.seat,"J9");
+    strcpy(theater.movieTitle,"Adit & Jarwo");
+    cout<< theater.room<< endl;
+    cout<< theater.seat<< endl;
+    cout<< theater.movieTitle<< endl;
+
+    return 0;
+}
